105f89f9f55a6c70cfc492df0e854cb81b101e21
Matej Šándor <matejko.sandor@gmail.com>
2020-05-17 20:09:45 +0200

    basic array implementation

---
+++ src/com/sandor/arrays/Arrays.java
@@ -0,0 +1,17 @@
+package com.sandor.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        // write to the array
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        //read values
+        for (int value : array) {
+            System.out.println(value);
+        }
+    }
+}
