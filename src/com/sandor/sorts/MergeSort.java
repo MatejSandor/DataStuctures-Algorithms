51f82e4327f7d785bd5719faeddd107052491eb2
Matej Šándor <matejko.sandor@gmail.com>
2020-05-28 14:29:43 +0200

    add splitting of the array in merge sort

--- src/com/sandor/sorts/MergeSort.java
+++ src/com/sandor/sorts/MergeSort.java
@@ -11,5 +11,19 @@ public class MergeSort {
             }
         }
     }
-    
+
+    public static void mergeSort(int[] input, int start, int end) {
+        if(end - start < 2) { return; }
+
+        int mid = (start + end) / 2;
+
+        mergeSort(input,start,mid);
+        mergeSort(input, mid, start);
+        merge(input,start,mid,end);
+
+    }
+
+    public static void merge(int[] input, int start, int mid, int end) {
+
+    }
 }
