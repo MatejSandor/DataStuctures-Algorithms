dec7de5194ac32127667957ba68247fc4e2eeac7
Matej Šándor <matejko.sandor@gmail.com>
2020-05-28 14:19:59 +0200

    start merge sort implementation

---
+++ src/com/sandor/sorts/MergeSort.java
@@ -0,0 +1,15 @@
+package com.sandor.sorts;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i]);
+            if(i != intArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+    
+}
