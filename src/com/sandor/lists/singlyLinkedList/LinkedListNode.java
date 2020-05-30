76e52c1a1f9f80898e42965ff4f0f52a05744bf6
Matej Šándor <matejko.sandor@gmail.com>
2020-05-30 13:22:33 +0200

    add singly linked list setup

---
+++ src/com/sandor/lists/singlyLinkedList/LinkedListNode.java
@@ -0,0 +1,28 @@
+package com.sandor.lists.singlyLinkedList;
+
+import com.sandor.lists.Employee;
+
+public class LinkedListNode {
+    private Employee employee;
+    private LinkedListNode next;
+
+    public LinkedListNode(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public LinkedListNode getNext() {
+        return next;
+    }
+
+    public void setNext(LinkedListNode next) {
+        this.next = next;
+    }
+}
---
+++ src/com/sandor/lists/singlyLinkedList/SinglyLinkedLists.java
@@ -0,0 +1,14 @@
+package com.sandor.lists.singlyLinkedList;
+
+import com.sandor.lists.Employee;
+
+public class SinglyLinkedLists {
+    public static void main(String[] args) {
+
+        Employee jane = new Employee("Jane", "Jones", 123);
+        Employee john = new Employee("John", "Doe", 4567);
+        Employee mary = new Employee("Mary", "Smith", 22);
+        Employee mike = new Employee("Mike", "Wilson", 3245);
+
+    }
+}
