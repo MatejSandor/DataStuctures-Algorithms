b0c055c77b8574a684bcadba73514eb43c642e40
Matej Šándor <matejko.sandor@gmail.com>
2020-05-31 18:20:49 +0200

    update put method (linear probing)

--- src/com/sandor/hashtables/HashTable.java
+++ src/com/sandor/hashtables/HashTable.java
@@ -15,7 +15,20 @@ public class HashTable {
 
     public void put(String key, Employee employee) {
         int hashedKey = hashKey(key);
-        if(hashtable[hashedKey] != null) {
+        if(occupied(hashedKey)) {
+            int stopIndex = hashedKey;
+            if(hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            } else {
+                hashedKey++;
+            }
+
+            while (occupied(hashedKey) && hashedKey != stopIndex) {
+                hashedKey = (hashedKey+1) % hashtable.length;
+            }
+        }
+
+        if(occupied(hashedKey)) {
             System.out.println("Sorry, the position is already occupied. :(");
         } else {
             hashtable[hashedKey] = employee;
@@ -25,5 +38,15 @@ public class HashTable {
     public Employee get(String key) {
         int hashedKey = hashKey(key);
         return hashtable[hashedKey];
+    }
+
+    public void printHashtable() {
+        for (Employee employee : hashtable) {
+            System.out.println(employee);
+        }
+    }
+
+    private boolean occupied(int index) {
+        return hashtable[index] != null;
     }
 }
