610ffd3930d892c494064d5faf7c62c43b2f0e47
Matej Šándor <matejko.sandor@gmail.com>
2020-05-31 17:46:21 +0200

    add hashtables setup

---
+++ src/com/sandor/hashtables/HashTable.java
@@ -0,0 +1,12 @@
+package com.sandor.hashtables;
+
+import com.sandor.Employee;
+
+public class HashTable {
+    Employee[] hashtable;
+
+    public HashTable(int capacity) {
+        hashtable = new Employee[capacity];
+    }
+    
+}
