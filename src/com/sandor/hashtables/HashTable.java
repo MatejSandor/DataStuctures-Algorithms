c662a95480faabb6d7c0a0fee550b9b79dcc0ad9
Matej Šándor <matejko.sandor@gmail.com>
2020-05-31 18:00:23 +0200

    add put function for hashtable

--- src/com/sandor/hashtables/HashTable.java
+++ src/com/sandor/hashtables/HashTable.java
@@ -12,4 +12,13 @@ public class HashTable {
     private int hashKey(String key) {
         return key.length() % hashtable.length;
     }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if(hashtable[hashedKey] != null) {
+            System.out.println("Sorry, the position is already occupied. :(");
+        } else {
+            hashtable[hashedKey] = employee;
+        }
+    }
 }
