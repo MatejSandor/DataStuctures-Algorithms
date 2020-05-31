423e02ca950dbeba7fba52d5b200544730554075
Matej Šándor <matejko.sandor@gmail.com>
2020-05-31 12:25:48 +0200

    add (array) queue implementation

--- src/com/sandor/queues/ArrayQueue.java
+++ src/com/sandor/queues/ArrayQueue.java
@@ -2,6 +2,8 @@ package com.sandor.queues;
 
 import com.sandor.Employee;
 
+import java.util.NoSuchElementException;
+
 public class ArrayQueue {
     private Employee[] queue;
     private int front;
@@ -12,22 +14,49 @@ public class ArrayQueue {
     }
 
     public int getSize() {
-        return back;
+        return back-front;
     }
 
     public boolean isEmpty() {
-        return back-front < 1;
+        return back-front == 0;
     }
 
     public void enqueue(Employee employee) {
+        if(back == queue.length) {
+            Employee[] newArray = new Employee[2*queue.length];
+            System.arraycopy(queue,0,newArray,0,queue.length);
+            queue = newArray;
+        }
 
+        queue[back++] = employee;
     }
 
-    public void dequeue() {
+    public Employee dequeue() {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
 
+        Employee employee = queue[front];
+        queue[front++] = null;
+
+        if(getSize() == 0) {
+            front = 0;
+            back = 0;
+        }
+
+        return employee;
     }
 
-    public void peek() {
+    public Employee peek() {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue[front];
+    }
 
+    public void printQueue() {
+        for(int i = front; i < back; i++) {
+            System.out.println(queue[i]);
+        }
     }
 }
