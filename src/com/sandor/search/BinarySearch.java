35a296c43e4e678090aafeb7d493a0240a9ae05a
Matej Šándor <matejko.sandor@gmail.com>
2020-06-01 15:33:14 +0200

    add recursive binary search implementation

--- src/com/sandor/search/BinarySearch.java
+++ src/com/sandor/search/BinarySearch.java
@@ -26,4 +26,23 @@ public class BinarySearch {
        }
        return -1;
     }
+
+    public static int recursiveBinarySearch(int[] input, int value) {
+        return recursiveBinarySearch(input,0,input.length,value);
+    }
+
+    public static int recursiveBinarySearch(int[] input,int start, int end, int value) {
+        if(start >= end) {
+            return -1;
+        }
+        int midpoint = (start+end)/2;
+
+        if(input[midpoint] == value) {
+            return midpoint;
+        } else if(value > input[midpoint]) {
+            return recursiveBinarySearch(input,midpoint+1,end,value);
+        } else {
+            return recursiveBinarySearch(input, start, midpoint, value);
+        }
+    }
 }
