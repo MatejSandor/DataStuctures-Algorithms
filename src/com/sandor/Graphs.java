25a1368e1ede90fea660cf79247d4bdb7839aac9
Matej Šándor <matejko.sandor@gmail.com>
2020-06-05 12:52:20 +0200

    add bubble sort description

--- src/com/sandor/Graphs.java
+++ src/com/sandor/Graphs.java
@@ -10,13 +10,12 @@ class Graphs {
     Graphs(int v) {
         V = v;
         adj = new LinkedList[v];
-        for (int i=0; i<v; ++i)
+        for (int i = 0; i < v; ++i)
             adj[i] = new LinkedList();
     }
 
     // Function to add an edge into the graph
-    void addEdge(int v,int w)
-    {
+    void addEdge(int v, int w) {
         adj[v].add(w);
     }
 
